ae9e35c50efbe05b4f17527a73a7806e7278072a
Raizer Varela <raizer.varela@hotmail.com>
2020-10-07 15:08:07 -0300

    Adição de exercício com Servlet

---
+++ LES_ExercicioTabuada/src/aplicacao_servlet/Tabuada.java
@@ -0,0 +1,33 @@
+package aplicacao_servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet ("/principal")
+public class Tabuada extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String strNumero = req.getParameter("NUMERO");
+		int numero = 7;
+		try {
+			numero = Integer.parseInt(strNumero);
+		} catch (Exception e){
+			//ignore
+		}
+		PrintWriter out = res.getWriter();
+		for(int i=0; i<=10; i++) {
+			int resultado = numero * i;
+			out.printf("<p>%d X %d = %d</p>%n", numero, i, resultado);
+		}
+		out.flush();
+	}
+
+}
+
